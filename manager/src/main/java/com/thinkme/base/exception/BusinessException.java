00c43f7d6c31bfb1c509e8048b57e0791cba4163
donotcoffee <donotcoffee@gmail.com>
2018-01-02 00:47:01 +0800

    feat:引入jsp的支持，提供jar 或war的打包形式

--- manager/src/main/java/com/thinkme/base/exception/BusinessException.java
+++ manager/src/main/java/com/thinkme/base/exception/BusinessException.java
@@ -4,7 +4,8 @@ package com.thinkme.base.exception;
 import com.thinkme.common.response.Response;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 /**
  * 自定义业务逻辑异常类
@@ -12,7 +13,8 @@ import lombok.Data;
  * <p>如果有更具体业务的自定义异常，请明确的定义，并继承此类</p>
  */
 @Builder
-@Data
+@Getter
+@Setter
 @AllArgsConstructor
 public class BusinessException extends RuntimeException {
 
--- manager/src/main/java/com/thinkme/controller/IndexController.java
+++ manager/src/main/java/com/thinkme/controller/IndexController.java
@@ -2,8 +2,10 @@ package com.thinkme.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * @author chenhaipeng
@@ -17,8 +19,14 @@ public class IndexController {
 
     @RequestMapping(value = "/")
     public String index() {
-//		System.out.println("hello world");
+//      System.out.println("hello world");
         logger.info("hello chen");
         return "hello thinkme";
+    }
+
+    @RequestMapping(value = "/jsp")
+    public ModelAndView index(ModelMap map) {
+        map.put("title", "HelloWorld");
+        return new ModelAndView("index");
     }
 }
